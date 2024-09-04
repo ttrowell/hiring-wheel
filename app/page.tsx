de3f4c25625655fa05a3d647df1aa74bccbca2f2
ttrowell <tara@rowellstreet.com>
2024-09-04 18:14:24 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import { PricingSimulator } from "@/components/pricing-simulator"
+
+export default function Page() {
+  return <PricingSimulator />
+}
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+//const nextConfig = {};
+const nextConfig = {
+    distDir: "build",
+    reactStrictMode: true,
+    swcMinify: true,
+};
+
+
+//export default nextConfig;
+
+
+
+module.exports = nextConfig
